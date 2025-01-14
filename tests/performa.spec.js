0b5c6813e53d8a83af8c2853393259d966ce596d
Dayezzz <adiasmuhsin1206@gmail.com>
2025-01-14 13:40:12 +0700

    detail test

---
+++ tests/performa.spec.js
@@ -0,0 +1,27 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("test kecepatan dari website", async ({ page }) => {
+    const start = Date.now(); 
+    await page.goto('https://selapan.smkn8malang.sch.id/login/index.php');
+    const end = Date.now(); 
+  
+    const loadTime = end - start; 
+    console.log(`Waktu muat halaman: ${loadTime} ms`);
+    expect(loadTime).toBeLessThan(3000);
+
+});
+test("test kecepatan dari login di website", async ({ page }) => {
+    const start = Date.now(); 
+    await page.goto('https://selapan.smkn8malang.sch.id/login/index.php');
+    await page.fill('input[name="username"]', "5240123663");
+    await page.fill('input[name="password"]', "MS5DRZEZ");
+    await page.click('button[type="submit"]');
+    await expect(page).toHaveURL("https://selapan.smkn8malang.sch.id/my/");
+    const end = Date.now(); 
+  
+    const loadTime = end - start; 
+    console.log(`Waktu muat halaman: ${loadTime} ms`);
+    expect(loadTime).toBeLessThan(3000);
+
+});
